f192d283e15af547e3756ad53e1cee0406360ba4
youngjr0527 <hipizza0527@gmail.com>
2023-06-30 04:45:22 +0000

    feat: bash shell method

--- anomaly_seg_list.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/python3 main.py --input_img_path mvtec/bottle/test/broken_large/006.png
-/usr/bin/python3 main.py --input_img_path mvtec/bottle/test/broken_large/007.png
-/usr/bin/python3 main.py --input_img_path mvtec/bottle/test/broken_large/011.png
-/usr/bin/python3 main.py --input_img_path mvtec/bottle/test/broken_large/012.png
